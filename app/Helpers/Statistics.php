a7383362801866a14c966d1147025283e97beb4e
Fitri Ali <mail@fitri.co>
2019-09-09 13:41:34 +0800

    Added countries to statistics helper

--- app/Helpers/Statistics.php
+++ app/Helpers/Statistics.php
@@ -9,35 +9,22 @@ class Statistics
 {
 	public static function link($link_id)
 	{
-		$hits = DB::table('hits')
-			->select(DB::raw('count(*) as count, DATE(created_at) as created'))
+		$stats['today'] = DB::table('hits')
 			->where('link_id', $link_id)
-			->whereRaw('DATE(created_at) > DATE_SUB(CURDATE(), INTERVAL 30 DAY)')
-			->groupBy('created')
-			->get();
-
-		foreach ($hits as $day)
-		{
-			$days[$day->created] = $day->count;
-		}
-
-		$day = Carbon::now()->subDay(29);
-
-		while ($day <= Carbon::now())
-		{
-			$stats['hits'][$day->format('jS F Y')] = $days[$day->format('Y-m-d')] ?? 0;
-			$day->addDay();
-		}
+			->whereRaw('DATE(created_at) = CURDATE()')
+			->count();
 
 		$stats['week'] = DB::table('hits')
 			->where('link_id', $link_id)
 			->whereRaw('DATE(created_at) > DATE_SUB(CURDATE(), INTERVAL 7 DAY)')
 			->count();
 
-		$stats['today'] = DB::table('hits')
+		$hits = DB::table('hits')
+			->select(DB::raw('count(*) as count, DATE(created_at) as created'))
 			->where('link_id', $link_id)
-			->whereRaw('DATE(created_at) = CURDATE()')
-			->count();
+			->whereRaw('DATE(created_at) > DATE_SUB(CURDATE(), INTERVAL 30 DAY)')
+			->groupBy('created')
+			->get();
 
 		$referrers = DB::table('hits')
 			->select(DB::raw('count(*) as count, referrer'))
@@ -47,9 +34,34 @@ class Statistics
 			->take(10)
 			->get();
 
+		$countries = DB::table('hits')
+			->select(DB::raw('count(*) as count, country'))
+			->where('link_id', $link_id)
+			->groupBy('country')
+			->orderBy('count', 'desc')
+			->take(10)
+			->get();
+
+		$day = Carbon::now()->subDay(29);
+		while ($day <= Carbon::now())
+		{
+			$stats['hits'][$day->format('jS F Y')] = $days[$day->format('Y-m-d')] ?? 0;
+			$day->addDay();
+		}
+
+		foreach ($hits as $day)
+		{
+			$days[$day->created] = $day->count;
+		}
+
 		foreach ($referrers as $referrer)
 		{
 			$stats['referrers'][$referrer->referrer] = $referrer->count;
+		}
+
+		foreach ($countries as $country)
+		{
+			$stats['country'][$country->country] = $country->count;
 		}
 
 		return $stats;
