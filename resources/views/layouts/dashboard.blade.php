c417dfc61f97fa09b8d1e403a7a937bd9c6a3c92
Fitri Ali <mail@fitri.co>
2019-09-11 10:36:07 +0800

    Installed and configured Tailwindcss

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,61 +3,52 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-        <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
+
     <body>
-        @guest
-        <a href="{{ route('login') }}">{{ __('Login') }}</a><br>
-        @if (Route::has('register'))
-        <a href="{{ route('register') }}">{{ __('Register') }}</a><br>
-        @endif
-        @else
-        {{ Auth::user()->name }}</br>
-        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-        </a>
+        <div class="max-w-md mx-auto px-4 bg-teal-100">
+            {{ Auth::user()->name }}</br>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
 
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-        @endguest
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
 
-        <h1>{{ config('app.name', 'Laravel') }}</h1>
-        
-        @if ($site_parameters['parent_url'])
-        <a href="{{ $site_parameters['parent_url'] }}">←</a>
-        @endif
+            <h1>{{ config('app.name', 'Laravel') }}</h1>
 
-        @if (session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-        @endif
-        @if (session('message'))
-        <div class="alert alert-info">{{ session('message') }}</div>
-        @endif
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+            @if ($site_parameters['parent_url'])
+            <a href="{{ $site_parameters['parent_url'] }}">←</a>
+            @endif
+
+            @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('message'))
+            <div class="alert alert-info">{{ session('message') }}</div>
+            @endif
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+            @endif
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+            @yield('content')
         </div>
-        @endif
-
-        @yield('content')
     </body>
 </html>
--- resources/views/link/create.blade.php
+++ resources/views/link/create.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
-    @if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-    </div>
-    @endif
-
-    <form action="/project" method="POST">
-        @csrf
-        <label for="name">Project Name</label>
-        <input type="text" name="name" maxlength="40" placeholder="Project Name" value="{!!old('name')!!}"/>
-        <label for="description">Description</label>
-        <input type="text" name="description" maxlength="160" placeholder="Give a brief description of your project" value="{!!old('description')!!}"/>
-        <button type="submit">Create</button>
-    </form>
-</div>
+<form action="/project" method="POST">
+    @csrf
+    <label for="name">Project Name</label>
+    <input type="text" name="name" maxlength="40" placeholder="Project Name" value="{!!old('name')!!}"/>
+    <label for="description">Description</label>
+    <input type="text" name="description" maxlength="160" placeholder="Give a brief description of your project" value="{!!old('description')!!}"/>
+    <button type="submit">Create</button>
+</form>
 @endsection
